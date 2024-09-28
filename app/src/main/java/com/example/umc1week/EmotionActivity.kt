c7b58d8541321920ede741708b132e24e616a24f
muunseo <mkogo90@naver.com>
2024-09-28 23:07:24 +0900

    1주차 코드

---
+++ app/src/main/java/com/example/umc1week/EmotionActivity.kt
@@ -0,0 +1,17 @@
+package com.example.umc1week
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+//onCreate는 액티비티가 생성될 때 호출되는 메서드
+class EmotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emotion)
+
+
+
+
+    }
+}
